25467d0bea1f96ef238cdd62dd6bb71348e6e422
RusseanFernandes <56703764+RusseanFernandes@users.noreply.github.com>
2024-04-04 16:35:25 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descição do curso java");
+        curso1.setCargaHoraria(8);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        curso2.setTitulo("Curso Java Script");
+        curso2.setDescricao("Descição do curso java Script");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now()); //Atribui a data de criação
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
